0b203e084ce4380c7464e57ad0695971804b3cd8
ibrahim <ibrahimitani@wireless140-210.wireless.wmich.edu>
2017-11-10 14:10:30 -0500

    Online Store HomePage

---
+++ OnlineStore/SignUp.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;  charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Online</title>
+
+    <link href="bootstrap3/dist/bootstrap.css" rel="stylesheet">
+
+    <link href="bootstrap3/dist/bootstrap-theme.css" rel="stylesheet">
+
+    <script src="bootstrap3/assets/js/html5shiv.js"></script>
+    <script src="bootstrap3/assets/js/respond.min.js"></script>
+
+</head>
+
+<body>
+
+
+
+
+
+
+</body>
+</html>
---
+++ OnlineStore/footer.php
@@ -0,0 +1,47 @@
+<footer>
+   <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+            <h4>More From Online Store</h4>
+            <ul class="nav nav-pills nav-stacked">
+                 <li><a href="#">Special Tokens</a></li>
+                 <li><a href="#">Accessories</a></li>
+                 <li><a href="#">Steam Gift Cards</a></li>
+                 <li><a href="#">Collectors Editions</a></li>
+               </ul>  
+        </div>
+        <div class="col-md-4">
+            <h4>Links</h4>
+            <nav>
+               <ul class="nav nav-pills nav-stacked">
+                 <li><a href="home.php">Home</a></li>
+                 <li><a href="#">About</a></li>
+                 <li><a href="#">Browse</a></li>
+                 <li><a href="#">Search</a></li>
+               </ul>            
+            </nav>               
+        </div>
+        <div class="col-md-4">
+            <h4>Contact Us</h4>
+            <hr/>
+
+            <div class="row">
+                <p class="glyphicon glyphicon-earphone"> Phone: 269-123-4444</p>
+                <p class="glyphicon glyphicon-envelope"> Email: support@OnlineStore.com</p>
+            </div>
+
+            <hr/>
+
+        </div>
+      </div>
+   </div>
+   <div class="container">
+      <hr/>
+      <div class="row">
+         <div class="col-md-6">
+            <p><small>&copy; 2017 - Online Store</small></p>
+         </div>
+
+      </div>
+   </div>
+</footer>
