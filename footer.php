7292b86662998ce540fc506e02696fef7a5b613a
mobyte0 <mobyte25@gmail.com>
2017-12-05 14:18:01 -0500

    revised footer

--- footer.php
+++ footer.php
@@ -1,46 +1,92 @@
-
 <footer class="panel-footer" style="bottom: 0;">
-   <div class="container">
-      <div class="row">
-        <div class="col-md-4">
-            <h4>More From Online Store</h4>
-            <ul class="nav nav-pills nav-stacked">
-                 <li><a href="FavoriteGames.php">Favorites</a></li>
-                <li><a href="Consoles.php">Consoles</a></li>
-                <li><a href="VideoGames.php">Video Games</a></li>
-               </ul>  
-        </div>
-        <div class="col-md-4">
-            <h4>Links</h4>
-            <nav>
-               <ul class="nav nav-pills nav-stacked">
-                 <li><a href="index.php">Home</a></li>
-                 <li><a href="about.php">About</a></li>
-                 <li><a href="#">Search</a></li>
-               </ul>            
-            </nav>               
-        </div>
-        <div class="col-md-4">
-            <h4>Contact Us</h4>
-            <hr/>
-
-            <div class="row">
-                <p class="glyphicon glyphicon-earphone"> Phone: 269-123-4567</p>
-                <p class="glyphicon glyphicon-envelope"> Email: support@OracleGameStore.com</p>
+    <div class="container" style="margin-top: 20px;">
+        <div class="col-md-4 col-sm-4 col-xs-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Store Links</h3>
+                </div>
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <a href="FavoriteGames.php">Favorites</a>
+                    </li>
+                    <li class="list-group-item">
+                        <a href="consoles.php">Consoles</a>
+                    </li>
+                    <li class="list-group-item">
+                        <a href="VideoGames.php">Games</a>
+                    </li>
+                </ul>
             </div>
-
-            <hr/>
-
         </div>
-      </div>
-   </div>
-   <div class="container">
-      <hr/>
-      <div class="row">
-         <div class="col-md-6">
-            <p><small>&copy; 2017 - Oracle Game Store</small></p>
-         </div>
-
-      </div>
-   </div>
+        <div class="col-md-4 col-sm-4 col-xs-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Oracle Links</h3>
+                </div>
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li class="list-group-item">
+                        <a href="about.php">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-4 col-xs-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Contact Us</h3>
+                </div>
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <span class="glyphicon glyphicon-earphone"></span> 269-867-5309
+                    </li>
+                    <li class="list-group-item">
+                        <span class="glyphicon glyphicon-envelope"></span> support@oraclegames.com
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!--      <div class="row">-->
+        <!--        <div class="col-md-4 col-sm-4">-->
+        <!--            <h4>More From Online Store</h4>-->
+        <!--            <ul class="nav nav-pills nav-stacked">-->
+        <!--                 <li><a href="FavoriteGames.php">Favorites</a></li>-->
+        <!--                <li><a href="Consoles.php">Consoles</a></li>-->
+        <!--                <li><a href="VideoGames.php">Video Games</a></li>-->
+        <!--               </ul>  -->
+        <!--        </div>-->
+        <!--        <div class="col-md-4 col-sm-4">-->
+        <!--            <h4>Links</h4>-->
+        <!--            <nav>-->
+        <!--               <ul class="nav nav-pills nav-stacked">-->
+        <!--                 <li><a href="index.php">Home</a></li>-->
+        <!--                 <li><a href="about.php">About</a></li>-->
+        <!--                 <li><a href="#">Search</a></li>-->
+        <!--               </ul>            -->
+        <!--            </nav>               -->
+        <!--        </div>-->
+        <!--        <div class="col-md-4 col-sm-4">-->
+        <!--            <h4>Contact Us</h4>-->
+        <!--            <hr/>-->
+        <!---->
+        <!--            <div class="row">-->
+        <!--                <p class="glyphicon glyphicon-earphone"> Phone: 269-123-4567</p>-->
+        <!--                <p class="glyphicon glyphicon-envelope"> Email: support@OracleGameStore.com</p>-->
+        <!--            </div>-->
+        <!---->
+        <!--            <hr/>-->
+        <!---->
+        <!--        </div>-->
+        <!--      </div>-->
+    </div>
+    <div class="container">
+        <hr/>
+        <div class="row">
+            <p style="text-align: center; color: #999;">
+                <small>&copy; 2017 - Oracle Game Store</small>
+            </p>
+        </div>
+    </div>
 </footer>
