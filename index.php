b9751bbebc4e7322160f0dac90c26e19a001e065
mobyte0 <mobyte25@gmail.com>
2017-12-05 12:59:05 -0500

    removed steam from carousel

--- index.php
+++ index.php
@@ -25,18 +25,11 @@
                                 <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
                                 <li data-target="#carousel-example-generic" data-slide-to="1"></li>
                                 <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="3"></li>
                             </ol>
 
                             <!-- Wrapper for slides -->
                             <div class="carousel-inner" role="listbox">
                                 <div class="item active">
-                                    <img src="images/steam.jpg" alt="..." style="width: 100%;">
-                                    <div class="carousel-caption">
-                                        Steam
-                                    </div>
-                                </div>
-                                <div class="item">
                                     <img src="images/ps4.jpg" alt="..." style="width: 100%;">
                                     <div class="carousel-caption">
                                         Playstation 4
